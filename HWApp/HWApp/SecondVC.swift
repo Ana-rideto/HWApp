ea395217b805cf984c7136649cb8f6cb8e7927eb
Ana-rideto <gaikaspb7@gmail.com>
2022-02-19 21:11:50 +0300

    HW added

---
+++ HWApp/HWApp/SecondVC.swift
@@ -0,0 +1,18 @@
+//
+//  SecondVC.swift
+//  HWApp
+//
+//  Created by Mac on 12.02.2022.
+//
+
+import Foundation
+import UIKit
+
+class SecondVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 1.00, green: 0.95, blue: 0.74, alpha: 1.00)
+        
+    }
+}
